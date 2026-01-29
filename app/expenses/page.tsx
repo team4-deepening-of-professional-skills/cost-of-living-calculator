65f4a9683846c5364da74747928b15b3d6ca98f6
t3ojen00 <t3ojen00@students.oamk.fi>
2026-01-29 23:38:21 +0200

    some kind of logic for inputs, needs some updates and delete

---
+++ app/expenses/page.tsx
@@ -0,0 +1,12 @@
+import FinancialInput from "./financialInputs";
+
+
+export default function Enni() {
+
+  return (
+    <div className="p-10">
+   <p> Input Screen</p>
+   <FinancialInput />
+    </div>
+  );
+}
